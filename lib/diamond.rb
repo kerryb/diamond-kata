ddefab0fdf3d5c93b24fb4d4a314e1add04d35a6
Kerry Buckley <kerryjbuckley@gmail.com>
2015-01-12 19:19:22 +0000

    Correct output for 'B'

--- lib/diamond.rb
+++ lib/diamond.rb
@@ -1,5 +1,13 @@
 class Diamond
   def print_diamond letter
-    letter
+    letters = ("A"..letter).to_a
+    max_gap = letters.size - 1
+    lines = letters.each_with_index.map {|l, i| line_for l, max_gap - i, i }
+    (lines + lines.reverse[1..-1]).join "\n"
+  end
+
+  private def line_for letter, indent, gap
+    return " " * indent + letter if gap.zero?
+    " " * indent + letter + " " * gap + letter
   end
 end
--- spec/diamond_spec.rb
+++ spec/diamond_spec.rb
@@ -4,4 +4,11 @@ describe Diamond do
   it "just prints A on its own" do
     expect(subject.print_diamond "A").to eq "A"
   end
+
+  it "prints a diamond for B" do
+    expect(subject.print_diamond "B").to eq "\
+ A
+B B
+ A"
+  end
 end
